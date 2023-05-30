985c098e243d1df8f20bc2e496c74de8745695af
Ankit Singh <ankitsingh91@gmail.com>
2023-05-30 14:54:42 +0530

    Basic Program

---
+++ 1-basics/1-basic-program.c
@@ -0,0 +1,26 @@
+/**
+ * @author Ankit Singh Chondawat
+ * @date 30th May 2023
+ * @details This file shows the basic program structure and terminologies for it.
+ */
+
+#include <stdio.h>
+// #include <stdio.h> - Standard Input Output Header File
+// Header file library - helps us to work with input/output functions
+
+int main()
+// main() main function - Most important part of the program (Entry Point of file)
+// main() - return value of function is integer
+
+{
+  printf("You are really old\n");
+  /* Executed if no other statement is */
+
+  return 0;  // returning value to int main()
+
+  /*
+  # 0           - Successful Exit of Program
+  # Non 0 value - Failure of Program
+  ;             - Statement Terminator
+  */
+}
--- _algorithms/1-even-odd.c
+++ _algorithms/1-even-odd.c
@@ -1,20 +1,12 @@
+/**
+ * @author Ankit Singh Chondawat
+ * @date 30th May 2023
+ */
+
 #include <stdio.h>
 
-// checkEvenOdd(int num) {
-//   if(num % 2 == 0) {
-//     printf('Number is even.');
-//   } else {
-//     printf('Number is odd.');
-//   }
-// }
-
-/**
- * Entry point of the file
-*/
 int main() {
-
   int num;
   scanf("%d", &num);
-
   printf("%d\nnum", &num);
 }
