c59d94589382bca7b9116015af79a3d1b8268b7d
Ankit Singh <ankitsingh91@gmail.com>
2023-05-30 14:56:18 +0530

    Comments

---
+++ 1-basics/2-comments.c
@@ -0,0 +1,19 @@
+/**
+ * @author Ankit Singh Chondawat
+ * @date 30th May 2023
+ * @details This file shows the comment format in C.
+ */
+
+#include <stdio.h>
+
+int main() {
+  // Single line comment
+
+  /*
+  Multi
+  line
+  comments
+  */
+
+  return 0;
+}
